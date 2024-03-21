2ec4f5aa1b61e08490d158975ecda029a609ba04
Yehor Boiar <yb33@st-andrews.ac.uk>
2024-03-21 12:51:38 +0000

    Added some files that has not been added in a previous commit

---
+++ dataPrep/createData.py
@@ -0,0 +1,51 @@
+import requests
+import pandas as pd
+import json
+
+
+df = pd.read_csv("data/Films.csv")
+
+names = []
+for i in range(len(df["Director"])//2):
+    # Check if the value is a string
+    if isinstance(df["Director"][i], str):
+        if "|" in df["Director"][i]:
+            separatedNames = df["Director"][i].split('|')
+            for name in separatedNames:
+                # Additional checks to ensure valid names
+                if all(char.isalpha() or char in "' " for char in name.strip()):
+                    names.append(name.strip())  # Strip leading/trailing whitespace and append
+        else:
+            # Additional checks to ensure valid names
+            if all(char.isalpha() or char in "' " for char in df["Director"][i].strip()):
+                names.append(df["Director"][i].strip())  # Strip leading/trailing whitespace and append
+# print(names[:10])
+# Print the list of names
+print(len(names))
+
+celebrities_data = []
+
+# API endpoint URL
+api_url_base = 'https://api.api-ninjas.com/v1/celebrity?'
+
+# API key
+api_key = 'htq3Qv20l9lWIsG6TcbaIQ==c7FOOXK80Udm1t9b'
+
+for name in names:
+    # Construct API URL with the name
+    api_url = api_url_base + 'name=' + name
+    response = requests.get(api_url, headers={'X-Api-Key': api_key})
+    
+    # Check if the request was successful
+    if response.status_code == requests.codes.ok:
+        celebrity_info = response.json()
+        
+        # Append the celebrity info to the list
+        celebrities_data.append(celebrity_info)
+    else:
+        print("Error:", response.status_code, response.text)
+
+# Write the collected data to a JSON file
+with open('celebrities_data2.json', 'w') as json_file:
+    json.dump(celebrities_data, json_file, indent=2)
+
---
+++ dataPrep/parseJSON.py
@@ -0,0 +1,34 @@
+import json
+import pandas as pd
+import csv
+
+# Load the JSON data from the file
+with open('celebrities_data.json', 'r') as json_file:
+    data = json.load(json_file)
+
+# Remove empty lists from the data
+data = [item for item in data if item]
+df = pd.read_json('celebrities_data.json')
+
+# Define the CSV file path
+csv_file_path = 'celebrities_data.csv'
+
+# Extract all unique keys from the JSON data
+unique_keys = ["name","gender","birthday","occupation","age","height","death","nationality","net_worth","is_alive"]
+
+# Write the data to a CSV file
+with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+    # Create a CSV writer object
+    writer = csv.DictWriter(csv_file, fieldnames=unique_keys)
+
+    # Write the header
+    writer.writeheader()
+
+    # Write the data rows
+    for item in data:
+        for entry in item:
+            writer.writerow(entry)
+
+
+
+
